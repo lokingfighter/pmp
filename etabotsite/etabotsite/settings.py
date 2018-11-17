437503b065d8ec2d514ed857837633d001de9ed9
lokingfighter <heyktoty@yahoo.com>
2018-11-17 14:18:49 -0800

    Update settings.py

--- etabotsite/etabotsite/settings.py
+++ etabotsite/etabotsite/settings.py
@@ -112,6 +112,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_jenkins',
     'etabotapp',
     'rest_framework',
     'rest_framework.authtoken',
